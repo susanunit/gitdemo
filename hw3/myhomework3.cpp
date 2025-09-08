e9c77eb68c590ee3f601a05a95fca4404c16489d
Susan Lindsey <slindsey@tacc.utexas.edu>
2025-09-08 17:45:53 -0500

    edits to hw3 homework

--- hw3/myhomework3.cpp
+++ hw3/myhomework3.cpp
@@ -6,4 +6,8 @@ using std::cout;
 int main (void) {
 
 	cout << "Hi from Susan\n";
+	cout << "latest update 09/08/25 17:34\n";
+
+	// I wrote a LOT of code, that I don't want to type in again
+	// solution: use github ! it's a repository
 }
